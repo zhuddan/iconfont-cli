433d48406f10ddaec5291d9bf358c7ff7e3854b2
zhuddan <yobaci@163.com>
2025-01-10 10:20:29 +0800

    1

--- demo/vue/ts/iconfont/iconfont-types.ts
+++
@@ -1,4 +0,0 @@
-/**
- * 图标名称
- */
-export type IconfontType = "loading" | "check-order" | "trade" | "connect" | "setup" | "coupon" | "order" | "notice" | "remark" | "navigate" | "user" | "phone" | "clock" | "chat" | "store" | "star" | "star-fill" | "close" | "shop" | "location" | "down" | "left" | "plus" | "right" | "search" | "up"
